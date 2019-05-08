a51da81b3b0fe20091dc3694ec2232d6e8d972e4
thule <lunthu@gmail.com>
2019-05-08 11:47:57 +0700

    conditional

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
 import React  from 'react'
 
 import PlusOne from './PlusOne'
+import Conditional from './Conditional'
 
 function App () {
   return (
     <div>
       <h2>Changing State</h2>
       <PlusOne />
+      <hr />
+      <h2>Conditinonal</h2>
+      <Conditional />
       <hr />
     </div>
   )
---
+++ src/components/Conditional.jsx
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+
+class Conditional extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isLoggIn: false
+    }
+    this.handleStatus = this.handleStatus.bind(this)
+  }
+
+  handleStatus() {
+    this.setState(prevState => {
+      return {
+        isLoggIn: !prevState.isLoggIn
+      }
+    })
+  }
+
+  render() {
+    return(
+      <div>
+        <strong>{this.state.isLoggIn ? "You're now LogIn" : "You need to LogIn"} </strong><br />
+        <button onClick={this.handleStatus}>{this.state.isLoggIn ? "LogOut" : "LogIn" }</button>
+      </div>
+      )
+  }
+}
+
+export default Conditional
